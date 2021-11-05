7d6ade1bd5d6d4d381805013c4ee8eebfa2513c0
Dmytro Momot <momotdev@gmail.com>
2021-11-05 15:16:53 +0200

    init commit

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import heroes from "../reducers/heroes";
+import filters from "../reducers/filters";
+
+const stringMiddleware = () => (next) => (action) => {
+	if (typeof action === 'string') {
+		return next({type: action})
+	} else {
+		return next(action);
+	}
+}
+
+const enhancer = (createStore) => (...args) => {
+	const store = createStore(...args);
+	const oldDispatch = store.dispatch;
+
+	store.dispatch = (action) => {
+
+	}
+	return store;
+}
+
+const store = createStore(
+	combineReducers({heroes,filters}),
+	compose(
+		applyMiddleware(ReduxThunk, stringMiddleware),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
+
+export default store;
